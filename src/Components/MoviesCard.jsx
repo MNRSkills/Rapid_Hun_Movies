e5fac76c3662324a42810f62681a2b211a8519b5
Mack Ramirez <mnrdev510@gmail.com>
2024-09-30 19:42:22 -0700

    Added styles to my card component

--- src/Components/MoviesCard.jsx
+++ src/Components/MoviesCard.jsx
@@ -6,17 +6,21 @@ const MoviesCard = ({ movie }) => {
   // console.log(movie, "this is props movie");
   const { title, thumbnail, description, image, rank } = movie;
   return (
-    <div className="border border-spacing-4 border-cyan-700 m-4 relative ease-out ">
-      <div className="hover:scale-125">
-        <img src={image} alt="" className=" cover " />
-      </div>
-
-      <div className="absolute top-0 bottom-0 mt-52 ml-10 w-full h-full bg-slate-500 hover:opacity-5 z-10">
-        <h1 className="text-center text-gray-600 font-bold text-3xl lg:max-w-80 z-10">
-          {title}
-        </h1>
-        <h2 className="text-4xl text-gray-700 font-bold px-25">{rank}</h2>
-        <button onClick={() => setFlip(true)}>Read More</button>
+    // main card here ------------
+    <div className="border border-spacing-4 border-cyan-700 m-4 relative ease-linear hover:scale-95 hover:opacity-50">
+      <img src={image} alt="" className="cover ease-linear hover:opacity-50" />
+      {/* ------------ */}
+      {/* Below is overlay */}
+      <div className="flex items-center justify-center flex-col absolute text-4xl text-white top-0 bottom-0 w-full h-full opacity-0 hover:opacity-100 ">
+        {/* top of overlay */}
+        <h1 className="text-center font-bold lg:max-w-80">{title}</h1>
+        <h2 className=" font-bold px-25">Rank:{rank}</h2>
+        <button
+          className="mt-10 px-4 py-2 bg-cyan-800 rounded shadow-lg opacity-100"
+          onClick={() => console.log("THIS WAS CLICKED")}
+        >
+          Read More
+        </button>
       </div>
     </div>
   );
